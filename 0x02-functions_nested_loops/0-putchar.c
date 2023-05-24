de810a227b32f9f87e6c90078a1972c3a5234a36
yousefelbadawy <114675087+Yousefelbadawy@users.noreply.github.com>
2023-05-24 04:54:01 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,16 @@
-#include "holberton.h"
-/**
- * main - prints the string "Holberton" from a character array.
- * Return: 0 on success.
- */
-int main(void)
-{
-	char c[] = "Holberton";
-	int i;
 
-	for (i = 0; i < 9; i++)
+#include "main.h"
+	#include <unistd.h>
+	/**
+	 * _putchar - writes the character c to stdout
+	 * @c: The character to print
+	 *
+	 * Return: On success 1.
+	 * On error, -1 is returned, and errno is set appropriately.
+	 */
+	int _putchar(char c)
 	{
-		_putchar(c[i]);
+		return (write(1, &c, 1));
 	}
-	_putchar('\n');
-	return (0);
-}
+
+
