19c45e6fe95ff8a7142508a193d163c758aff5ee
yousefelbadawy <114675087+Yousefelbadawy@users.noreply.github.com>
2023-06-22 04:45:57 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,4 +1,3 @@
-
 #include "3-calc.h"
 
 int op_add(int a, int b);
@@ -32,7 +31,12 @@ int op_sub(int a, int b)
 /**
  * op_mul - Returns the product of two numbers.
  * @a: The first number.
- * @b: The second number.{
+ * @b: The second number.
+ *
+ * Return: The product of a and b.
+ */
+int op_mul(int a, int b)
+{
 	return (a * b);
 }
 /**
@@ -57,19 +61,3 @@ int op_mod(int a, int b)
 {
 	return (a % b);
 }
-
-
-
-
-
-
-
-
-
-
-
-
- *
- * Return: The product of a and b.
- */
-int op_mul(int a, int b)
