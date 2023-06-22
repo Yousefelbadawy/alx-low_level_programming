c9b064b62cc347109617083b9eef78ba3cd08493
yousefelbadawy <114675087+Yousefelbadawy@users.noreply.github.com>
2023-06-22 04:41:35 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -32,7 +32,43 @@ int op_sub(int a, int b)
 /**
  * op_mul - Returns the product of two numbers.
  * @a: The first number.
+ * @b: The second number.{
+	return (a * b);
+}
+/**
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
  * @b: The second number.
+ *
+ * Return: The quotient of a and b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The remainder of the division of a by b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
+
+
+
+
+
+
+
+
+
+
+
  *
  * Return: The product of a and b.
  */
